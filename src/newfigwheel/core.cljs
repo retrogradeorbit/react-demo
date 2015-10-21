109a0c0e64c57b142ecee6426c0e80e3841e85d3
Crispin Wellington <retrogradeorbit@gmail.com>
2015-10-21 20:58:11 +0800

    rename some confusing vars

--- src/newfigwheel/core.cljs
+++ src/newfigwheel/core.cljs
@@ -17,10 +17,10 @@
 (defn select-cell-range
   "given a state atom value, returns a new value where
   all the rows from start to end have their :selected? key
-  set to value"
-  [state start end value]
+  set to 'destination'"
+  [state start end destination]
   (reduce
-   #(assoc-in %1 [:rows %2 :selected?] value)
+   #(assoc-in %1 [:rows %2 :selected?] destination)
    state
    (range (min start end) (inc (max start end)))))
 
@@ -28,22 +28,22 @@
   (let [state @table-atom]
     [:table {:style {:-webkit-user-select "none" :user-select "none"}}
      (map
-      (fn [key]
-        (let [{:keys [selected? content]} ((state :rows) key)]
-          ^{:key key}
+      (fn [row-id]
+        (let [{:keys [selected? content]} ((state :rows) row-id)]
+          ^{:key row-id}
           [:tr [:td
                 {:style {:background (if selected? "green" "none")}
                  :on-mouse-down #(let [destination (not selected?)]
                                    ;; destination holds what selected? should be set to. click-start remebers cell
-                                   (swap! ui-atom assoc :click-start key :destination destination)
+                                   (swap! ui-atom assoc :click-start row-id :destination destination)
                                    ;; select this cell so click immediately selects
-                                   (swap! table-atom assoc-in [:rows key :selected?] destination))
+                                   (swap! table-atom assoc-in [:rows row-id :selected?] destination))
                  :on-mouse-enter #(let [{:keys [click-start destination]} @ui-atom]
                                     (when click-start
                                       ;; select from the click-start through to the present cell
-                                      (swap! table-atom select-cell-range click-start key destination)
+                                      (swap! table-atom select-cell-range click-start row-id destination)
                                       ;; set click-start now to this cell to avoid re selecting cells over and over
-                                      (swap! ui-atom assoc :click-start key)))
+                                      (swap! ui-atom assoc :click-start row-id)))
                  :on-mouse-up #(swap! ui-atom dissoc :click-start :destination)}
                 content]]))
       (-> state :rows keys sort))]))
